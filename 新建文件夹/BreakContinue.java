cd9140a9ac10219228567a39035aecd08d00876b
remoolB <chengzixian1201@gmail.com>
2024-11-20 10:54:13 +0800

    lab1

---
+++ 新建文件夹/BreakContinue.java
@@ -0,0 +1,19 @@
+public class BreakContinue {
+    public static void windowPosSum(int[] a, int n) {
+        for (int i = 0; i < 6; ++i) {
+            if (a[i] < 0) continue;
+            for (int j = i + 1; j <= i + n && j <= 5;++j) {
+                a[i] += a[j];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, -3, 4, 5, 4};
+        int n = 3;
+        windowPosSum(a, n);
+
+        // Should print 4, 8, -3, 13, 9, 4
+        System.out.println(java.util.Arrays.toString(a));
+    }
+}
